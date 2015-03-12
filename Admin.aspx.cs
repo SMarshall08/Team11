473d6e47d9ce3a09c407e776fd08a2d4b2b83ff5
Woolfe <woolfey@hotmail.co.uk>
2015-03-12 01:48:17 +0000

    text box and button added - jonah
    A text box and button is generated for adding facility, sql not written yet.

--- Admin.aspx.cs
+++ Admin.aspx.cs
@@ -8,25 +8,23 @@ using System.Data.SqlClient;
 using System.Configuration;
 using System.Web.Configuration;
 using System.Text.RegularExpressions;
+
 namespace Team11
 {
     public partial class Admin : System.Web.UI.Page
     {
-        /// <summary>
-        /// Handles the Load event of the Page control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        int userID = 0;
         protected void Page_Load(object sender, EventArgs e) {
 
             SqlConnection connect = new SqlConnection(WebConfigurationManager.ConnectionStrings["AdminConnectionString"].ToString());
             connect.Open();
-           
             
+            userID = Convert.ToInt32(Request.QueryString["userID"]);
+            /*
             string url = Request.Url.Query; ///Gets url
             Match match = Regex.Match(url, @"=(.*)"); ///Gets user ID from url
-            string urlID = match.Groups[1].Value;
-            string adminString = "Select administrator from [User] where userID = " + urlID;
+            string urlID = match.Groups[1].Value;       */
+            string adminString = "Select administrator from [User] where userID = " + userID;
             SqlCommand adminCommand = new SqlCommand(adminString, connect);
             string administratorYesNo = adminCommand.ExecuteScalar().ToString();
             string trimmedAdmin = administratorYesNo.Trim();
@@ -36,14 +34,32 @@ namespace Team11
             if (userIsAdmin)
          {        //Below executed if user logged in is an admin.
 
-               areYouAdmin.InnerHtml="You are an admin.";
 
+
+             TextBox facilityText = new TextBox(); //This is the box that will have the facility entered into.
+             facilityText.Text = "Type facility name here.";
+             facilityText.ID = "que1";
+             areYouAdmin.Controls.Add(facilityText);
+             Button addFacility = new Button(); //This button will submit the facility.
+             addFacility.ID = "addFacility";
+             addFacility.Text = "Add Facility";
+             addFacility.Click += new EventHandler(addFacilityFunction);
+             areYouAdmin.Controls.Add(addFacility);
+             
+
+             
+
+             
+             
            }
            else {
                 //Below executed if user logged in is not an admin.
 
-               areYouAdmin.InnerHtml = "You are not an admin.";
+               areYouAdmin.InnerHtml = "<script>document.GetElementById(areYouAdmin).style.visibility=\"hidden\"</script>";
+               
                  }
+
+           
 
 
             connect.Close();
@@ -51,11 +67,17 @@ namespace Team11
         
         }
 
-        /// <summary>
-        /// Handles the Click event of the ButtonLogin control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void addFacilityFunction(Object sender, EventArgs e) //This is executed when the add facility button is pressed.
+        {
+
+            
+            scriptDiv.InnerHtml = "<script>alert(\"Facility successfully added!\")</script>";
+        
+        
+        }
+                                 
+
+        
         
     }
 }
