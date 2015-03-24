05ee13036dd2287e5b356f907e07951839edcff8
Woolfe <woolfey@hotmail.co.uk>
2015-03-24 05:25:57 +0000

    the page now shows you what facilities a room does and doesn't have, but you can't edit it yet

--- Facilities.aspx.cs
+++ Facilities.aspx.cs
@@ -35,6 +35,49 @@ namespace Team11
 
 
                 labelID.Text = "You are editing the facilities for room " + roomName + ". If this is not correct, you can ";
+
+                string listSQL = "SELECT facilityID FROM Facility";
+
+                SqlConnection deleteFacilitiesConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["AdminConnectionString"].ToString());
+                SqlCommand cmd = new SqlCommand(listSQL, deleteFacilitiesConnection);
+                SqlDataReader reader;
+                
+                var facilityIDs = new List<string>();
+                deleteFacilitiesConnection.Open();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {  
+                   
+                   string tempID = reader["facilityID"].ToString();
+                   facilityIDs.Add(tempID);
+                }
+                reader.Close();
+
+
+                string listSQL2 = "SELECT facilityID FROM RoomFacilities WHERE roomID ="+roomID;
+
+                SqlConnection deleteFacilitiesConnection2 = new SqlConnection(WebConfigurationManager.ConnectionStrings["AdminConnectionString"].ToString());
+                SqlCommand cmd2 = new SqlCommand(listSQL2, deleteFacilitiesConnection2);
+                SqlDataReader reader2;
+
+                var roomsFacilityIDs = new List<string>();
+                deleteFacilitiesConnection2.Open();
+                reader2 = cmd2.ExecuteReader();
+                while (reader2.Read())
+                {
+
+                    string tempID = reader2["facilityID"].ToString();
+                    roomsFacilityIDs.Add(tempID);
+                }
+                reader2.Close();
+                
+                var facilitiesTheRoomDoesNotHave = new List<string>();
+                facilitiesTheRoomDoesNotHave = (facilityIDs.Except(roomsFacilityIDs)).ToList();
+                String joined1 = String.Join(", ", roomsFacilityIDs);
+                String joined2 = String.Join(", ", facilitiesTheRoomDoesNotHave);
+                labelID2.Text = "The room " + roomName + " has facilities " + joined1 + ". It does not have facilities " + joined2 + ".";
+                
+
             }
 
         }
