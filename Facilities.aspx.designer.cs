d455f0b1f9dbb123ccf3ddb72f5ca2aef7f64b74
Woolfe <woolfey@hotmail.co.uk>
2015-03-24 05:26:16 +0000

    the page now shows you what facilities a room does and doesn't have, but you can't edit it yet

--- Facilities.aspx.designer.cs
+++ Facilities.aspx.designer.cs
@@ -29,5 +29,14 @@ namespace Team11 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label labelID;
+        
+        /// <summary>
+        /// labelID2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label labelID2;
     }
 }
