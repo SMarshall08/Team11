9fd9ae6a4c5cc53ac659bbb208f60f45ebfed1a2
benhyslop <b.d.hyslop-13@student.lboro.ac.uk>
2015-04-13 22:13:44 +0100

    Change Password Update
    Validation + error msgs working on change p/w page, just UPDATE SQL and
    '*' over p/w's left to do

--- ChangePassword.aspx
+++ ChangePassword.aspx
@@ -46,7 +46,7 @@
         <div class="canistercontainer">
             <div class="row">
                 <div class="text-center col-md-4 col-sm-4">
-                    <h3>Current Password</h3>
+                    <h3>Current Password:</h3>
                 </div>
                 <div class="text-center col-md-8 col-sm-8">
                    <asp:TextBox ID="CurrentPassTextBox" runat="server" Width="130px"></asp:TextBox>
@@ -54,18 +54,27 @@
             </div>
             <div class="row">
                 <div class="text-center col-md-4 col-sm-4">
-                    <h3>New Password</h3>
+                    <h3>New Password:</h3>
                 </div>
                 <div class="text-center col-md-8 col-sm-8">
-                    <asp:TextBox ID="NewPassTextBox" runat="server" Width="130px"></asp:TextBox>
+                   <asp:TextBox ID="NewPassTextBox" runat="server" Width="130px"></asp:TextBox>
+                </div>
+            </div>
+            <div class="row">
+                <div class="text-center col-md-4 col-sm-4">
+                    <h3>Confirm New Password:</h3>
+                </div>
+                <div class="text-center col-md-8 col-sm-8">
+                    <asp:TextBox ID="NewPassTextBox2" runat="server" Width="130px"></asp:TextBox>
                 </div>
             </div>
             <div class="row">
                 <div class="text-center col-md-6 col-sm-6 col-md-offset-3 col-sm-offset-3">
-                    <asp:Button class="btn btn-success btn-block" ID="Button1" runat="server" Text="Save" />
+                    <asp:Button class="btn btn-success btn-block" ID="Button1" runat="server" Text="Save" OnClick="Button1_Click" />
                 </div>
             </div>
         </div>
+        <asp:Label class="center errormessage" style="color:red;" ID="ErrorLabel" Text="" runat="server"></asp:Label>
     </div>
     
 
--- ChangePassword.aspx.cs
+++ ChangePassword.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Configuration;
+using System.Text.RegularExpressions;
 
 namespace Team11
 {
@@ -12,17 +16,50 @@ namespace Team11
         int userID = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
-            // SqlConnection connect = new SqlConnection(WebConfigurationManager.ConnectionStrings["AdminConnectionString"].ToString());
-            //connect.Open();
-
             userID = Convert.ToInt32(Session["userID"]);
-            Label4.Text = userID.ToString();
 
+        }
 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ErrorLabel.Text = " ";
+            //Clear the label holding msg for user
 
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["myConnectionString"].ToString())){
+                conn.Open();
+                // look up the users password so it can be compared to the entered value
+                string checkpassword = String.Format("Select password from [User] where userId={0}", userID);
+                SqlCommand passwordCmd = new SqlCommand(checkpassword, conn);
+                // Gets rid of the space if there is one e.g. by habit putting a space at the end
+                string password = passwordCmd.ExecuteScalar().ToString().Replace(" ", "");
+                conn.Close();
 
+                        
+                
+                if (password == CurrentPassTextBox.Text){
+                       
 
+                    if (NewPassTextBox.Text == NewPassTextBox2.Text){
+                        if (NewPassTextBox.Text.Length >= 6){
+                            //SQL Updating password goes into here
+                            ErrorLabel.Text = "Password has been updated. (Hasn't really, UPDATE SQL not in yet)";
+                        }
+                        else{
+                            //***Error for when not longer than 6 chars***
+                            ErrorLabel.Text = "Error - Your new password is not longer than 6 characters.";
+                        }
+                    }
+                    else{
+                        //***Error for entered two different new passwords***
+                        ErrorLabel.Text = "Error - You have entered two different new passwords.";
+                    }          
+                }
+                else{ 
+                    //Incorrect password error msg goes here
+                    ErrorLabel.Text = "Error - The current password entered is incorrect.";
+                }
 
+            }
         }
     }
 }
